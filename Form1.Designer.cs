06c8d2c20825c52df34e90bc12b32f7be2cac8ee
LauMir <42898270+LauMir@users.noreply.github.com>
2020-11-16 22:04:24 -0600

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ComprasORM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lista = new System.Windows.Forms.DataGridView();
+            this.Lista2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Lista)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lista2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Lista
+            // 
+            this.Lista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Lista.Location = new System.Drawing.Point(67, 56);
+            this.Lista.Name = "Lista";
+            this.Lista.Size = new System.Drawing.Size(342, 186);
+            this.Lista.TabIndex = 3;
+            // 
+            // Lista2
+            // 
+            this.Lista2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Lista2.Location = new System.Drawing.Point(67, 264);
+            this.Lista2.Name = "Lista2";
+            this.Lista2.Size = new System.Drawing.Size(342, 150);
+            this.Lista2.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "clientes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 245);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "lentes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 466);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Lista2);
+            this.Controls.Add(this.Lista);
+            this.Name = "Form1";
+            this.Text = "ORM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Lista)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lista2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView Lista;
+        private System.Windows.Forms.DataGridView Lista2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComprasORM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (ComprasEntities db = new ComprasEntities())
+            {
+                /*Cliente nuevoCliente = new Cliente();
+                nuevoCliente.IdCliente = Convert.ToInt32(11);
+                nuevoCliente.Nombre = "Abelina Lesper";
+                nuevoCliente.Direccion = "Roma #1050";
+                db.Cliente.Add(nuevoCliente);
+                db.SaveChanges();*/
+
+                var clientes = db.Cliente.OrderBy(dato => dato.Nombre);
+                Lista.DataSource = clientes.ToList();
+
+                /*var clientes2 =(from dato in db.Cliente
+                                join 
+                                from
+                                where
+                                select new
+                                {
+                                    TimeLeft= dato.StartTime*69*60*24-db.
+                                }).Cliente.OrderBy(dato => dato.TimeLeft);*/
+
+
+
+
+
+
+            }
+
+            using (ComprasEntities db2 = new ComprasEntities())
+            {
+                var lentes = from dato in db2.Lente
+                             where (dato.Material == "Pasta" && dato.Precio < 3000)
+                             orderby dato.Marca
+                             select dato;
+
+                Lista2.DataSource = lentes.ToList();
+
+            }
+        }
+    }
+}
